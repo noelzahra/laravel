6b074bb9b9529066b1021a7e57a3d6952f8ffb62
noelart <noelart@maltanet.net>
2017-10-05 23:56:50 +0200

    Buikd remote comtrollers

--- test/database/migrations/2017_10_05_185714_create_tasks_table.php
+++ test/database/migrations/2017_10_05_185714_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('usr_id');
             $table->string('title');
             $table->text('body');
             $table->timestamps();
--- test/routes/web.php
+++ test/routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Task;
+
 
 Route::get('/', function () {
     $name = 'Scott';
@@ -28,18 +20,26 @@ Route::get('/', function () {
 // });
 
 //Populate Tasks collection from db
-Route::get('/tasks', function() {
-    $tasks = DB::table('tasks')->latest()->get();
+// Route::get('/tasks', function() {
+//     //$tasks = DB::table('tasks')->latest()->get();
+//     $tasks = Task::all(); //eloquent method
 
-    return view('tasks.index', ['tasks' => $tasks]);
-});
+//     return view('tasks.index', ['tasks' => $tasks]);
+// });
 
-//Get one task by id
-Route::get('/tasks/{task}', function($id) {
-    $task = DB::table('tasks')->find($id);
 
-    return view('tasks.show', ['task' => $task]);
-});
+
+// //Get one task by id
+// Route::get('/tasks/{task}', function($id) {
+//     //$task = DB::table('tasks')->find($id);
+
+//     $task = Task::find($id);
+//     return view('tasks.show', ['task' => $task]);
+// });
+
+Route::get('/tasks', 'TasksController@index');
+
+Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/about', function() {
     return view('about');
